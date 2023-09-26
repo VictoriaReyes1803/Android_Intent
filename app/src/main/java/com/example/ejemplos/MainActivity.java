966b62ce66db33ae68ece8929f47da8bcfc04e33
VictoriaReyes1803 <139159900+VictoriaReyes1803@users.noreply.github.com>
2023-09-26 12:15:42 -0600

    Android Intent

---
+++ app/src/main/java/com/example/ejemplos/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ejemplos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TintableCheckedTextView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+      Button B;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        B = (Button)  findViewById(R.id.btn1);
+        B.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
+
+}
